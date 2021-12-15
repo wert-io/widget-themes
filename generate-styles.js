965fe3217ad785da4748c5974e9e694ac666db33
apples-father <kostya@wert.io>
2021-12-15 13:36:44 +0300

    Stylus file generation added

---
+++ generate-styles.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const path = require('path');
+// const stylus = require('stylus');
+const {
+  defaultSemanticColors,
+  makeThemeFromSemantic,
+} = require('./index.js');
+
+const writeDirectory = './styles';
+const filename = 'set-theme-var.styl';
+const defaultTheme = makeThemeFromSemantic(defaultSemanticColors);
+
+const content = `$theme = {
+  ${Object
+    .entries(defaultTheme)
+    .map(([key, color]) => `"${key}": ${color}`).join(',\n  ')}
+}`;
+
+if (!fs.existsSync(writeDirectory)){
+  fs.mkdirSync(writeDirectory, { recursive: true });
+}
+
+fs.writeFileSync(path.join(writeDirectory, filename), content);
